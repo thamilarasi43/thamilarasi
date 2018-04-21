ffce8a0dfb49a86db3c3d5e25ef0876f3b789b1b
thamilarasi43 <37330896+thamilarasi43@users.noreply.github.com>
2018-04-21 15:13:50 +0530

    Create p13.py

---
+++ p13.py
@@ -0,0 +1,14 @@
+n1=int(input("enter n"))
+q=int(input("enter q"))
+l=[]
+l1=[]
+for i in range(n1):
+    a=int(input("enter n val"))
+    l.append(a)
+for j in range(q):
+    c=0
+    u=int(input())
+    v=int(input())
+    for i in range(u,v):
+        l1.append(l)
+    print(min(l))
